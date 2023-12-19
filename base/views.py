56246ccf423169c67da90226c2b2de4d321f0e5c
samkrdev <sampath.mummaneni@gmail.com>
2023-12-19 19:46:54 +0530

    Done

--- base/views.py
+++ base/views.py
@@ -2,14 +2,17 @@ from calendar import c
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from base.models import Room
+
 # Create your views here.
-rooms = [
-    {'id':1,'name':'Lets Learn Python','instructor':'John Doe','category':'Programming','image':'https://source.unsplash.com/1600x900/?coding'},
-    {'id':2,'name':'Lets Learn Java','instructor':'John Doe','category':'Programming','image':'https://source.unsplash.com/1600x900/?coding'},
-    {'id':3,'name':'Lets Learn C++','instructor':'John Doe','category':'Programming','image':'https://source.unsplash.com/1600x900/?coding'},
-]
+# rooms = [
+#     {'id':1,'name':'Lets Learn Python','instructor':'John Doe','category':'Programming','image':'https://source.unsplash.com/1600x900/?coding'},
+#     {'id':2,'name':'Lets Learn Java','instructor':'John Doe','category':'Programming','image':'https://source.unsplash.com/1600x900/?coding'},
+#     {'id':3,'name':'Lets Learn C++','instructor':'John Doe','category':'Programming','image':'https://source.unsplash.com/1600x900/?coding'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,"base/home.html",context=context)
     # return HttpResponse("Home Page")
@@ -19,10 +22,7 @@ def home(request):
 #     # return HttpResponse("This is a room")
 
 def room(request,pk):
-    room_ = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room_ = i
-    context = {'room':room_}
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
     return render(request,"base/room.html",context=context)
     # return HttpResponse("This is a room")
